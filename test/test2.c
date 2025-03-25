919856ba15ce149c1d1b09e374cb46311e08e237
vaibhavi <vaibhavim081203@gmail.com>
2025-03-25 11:14:56 +0530

    initial semantic analysis

--- test/test2.c
+++ test/test2.c
@@ -1,5 +1,27 @@
-int main(){
-
-    volatile int x= 5;
-    const char y=1;
+int main()
+{
+	int t; 
+	scanf("%d",&t);
+	while(t--)
+	{
+    	int a,b;
+    	int i; 
+	long  ** s=0,za=a;
+    	scanf("%d %d",&a,&b);
+	for(i=a;i<=b;i++)
+    	{
+        	int reverse=0;
+		int rem;
+		int temp=i;
+        	while(temp!=0)
+            {
+                rem=temp%10;
+                reverse=reverse*10+rem;
+                temp/=10;
+            }
+            if(reverse==i) s+=i;
+        }
+    	printf("%ld\n",s);
+    }
+    return 0;
 }
